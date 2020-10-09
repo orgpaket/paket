abdd2d2146759cc1b4221ef9ddb1144dc2cd516a
Jayesh Badwaik <jayesh@badwaik.in>
2020-10-09 14:01:17 +0530

    + paket: added paket binary

---
+++ src/bin/paket.cpp
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// SPDX-License-Identifier: "Apache-2.0 OR MIT"
+// Copyright (C) 2020, Jayesh Badwaik <jayesh@badwaik.in>
+//------------------------------------------------------------------------------
+
+#include <cstdlib>
+
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) -> int
+{
+  return EXIT_SUCCESS;
+}
